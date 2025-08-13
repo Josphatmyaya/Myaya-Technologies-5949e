803b2d14729c72d846bdfee7f1da0c99f44c3769
Josphatmyaya <Josphatmyaya@hotmail.com>
2025-08-13 04:28:49 +0200

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'MYAYA TECHNOLOGIES';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'MYATA JOSPHAT';
+    : 'EXPERIMENTAL PROJECT';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
