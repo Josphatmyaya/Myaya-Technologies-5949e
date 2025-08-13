92734aa281c5335444bcf2870936cc4f2df25a8d
Josphatmyaya <Josphatmyaya@hotmail.com>
2025-08-13 04:10:11 +0200

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'MYAYA TECHNOLOGIES';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'MYATA JOSPHAT';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
